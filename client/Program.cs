c42c88a05faf274d9ec25611e216b5081d6f5b73
George Margiev <george.margiev.04@gmail.com>
2024-10-06 18:46:44 +0300

    feat: create web and client apps

---
+++ client/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string res = GetString("https://localhost:44347/currencies");
+        }
+
+        static public string GetString(string requestUrl)
+        {
+            string res = string.Empty;
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    res = client.GetAsync(requestUrl).Result.Content.ReadAsStringAsync().Result;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
+
+            return res;
+        }
+    }
+}
---
+++ webAPI/Controllers/BankController.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Runtime.InteropServices;
+using System.Web.Http;
+using System.Web.Http.Results;
+using webAPI.Models;
+
+namespace webAPI.Controllers
+{
+    public class BankController : ApiController
+    {
+
+        //GET
+        [HttpGet, Route("currencies")]
+        public IEnumerable<Currency> currencies()
+        {
+            Currency CurUSD = new Currency();
+            CurUSD.Name = "USD";
+            CurUSD.ID = 1;
+
+            Currency CurRUB = new Currency();
+            CurRUB.Name = "RUB";
+            CurRUB.ID = 2;
+
+            List<Currency> CurLst = new List<Currency>();
+            CurLst.Add(CurUSD);
+            CurLst.Add(CurRUB);
+
+            return CurLst;
+
+        }
+
+
+        [HttpGet, Route("currencies/{id}")]
+        public Currency currency(int id)
+        {
+            Currency CurUSD = new Currency();
+            CurUSD.Name = "USD";
+            CurUSD.ID = id;
+            // поиск по бд
+            // ...
+            //
+            return CurUSD;
+        }
+
+
+        [HttpPost, Route("currencies/new/{id}/{name}")]
+        public IHttpActionResult newCurrency(int id, string name)
+        {
+            // звпись в бд
+            // ...
+            //
+
+            string info = Request.Content.ReadAsStringAsync().Result;
+
+            if (id == 1)
+            return Ok();
+            else
+                return BadRequest();
+        }
+    }
+}
